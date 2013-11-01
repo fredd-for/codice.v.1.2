8375be70ea094c4c929d4587f589e464f2835a3e
fredd-for <fredd_for@hotmail.com>
2013-11-01 12:36:08 -0400

    ingresar datos

---
+++ application/logs/2013/10/30.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-10-30 10:31:22 --- ERROR: Database_Exception [ 0 ]: [1049] Unknown database 'correspondencia_mmaya' ~ MODPATH/database/classes/kohana/database/mysql.php [ 96 ]
